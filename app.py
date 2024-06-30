e826db6f95beb77e69b2302ad11dfb39bff78c76
Samriddhi Mishra <sam14.diva@gmail.com>
2024-06-30 16:28:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+import pickle
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+app = Flask(__name__)
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    
+    session = requests.Session()
+    retry = Retry(
+        total=5,
+        read=5,
+        connect=5,
+        backoff_factor=0.3,
+        status_forcelist=(500, 502, 504)
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    
+    try:
+        response = session.get(url, timeout=30)  # Increased timeout
+        response.raise_for_status()
+        data = response.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+        else:
+            print(f"No poster path found for movie_id {movie_id}")
+            return "Poster not available"
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return "Poster not available"
+
+def recommend(movie):
+    print("In recommend function")
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        poster = fetch_poster(movie_id)
+        recommended_movie_posters.append(poster)
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    print("Recommendations generated")
+    return recommended_movie_names, recommended_movie_posters
+
+# Load the model and data
+print("Loading models")
+movies = pickle.load(open('model/movie_list.pkl', 'rb'))
+similarity = pickle.load(open('model/similarity.pkl', 'rb'))
+print("Models loaded")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print("In index function")
+    movie_list = movies['title'].values
+    recommendations = []
+    if request.method == 'POST':
+        print("Form submitted")
+        selected_movie = request.form['movie']
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+        recommendations = zip(recommended_movie_names, recommended_movie_posters)
+    return render_template('index.html', movie_list=movie_list, recommendations=recommendations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
